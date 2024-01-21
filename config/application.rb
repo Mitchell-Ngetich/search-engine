741bde54777ea9c9b37c108c6333ae9bea1cf882
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2024-01-21 15:23:36 +0300

    configuring render

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative "boot"
 
 require "rails/all"
+require 'dotenv/rails-now'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
